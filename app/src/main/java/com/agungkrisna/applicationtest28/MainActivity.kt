306a91b3da7fe31ced6c53556643a1b65c8eec86
gungdekrisna <gungkrisna31@gmail.com>
2022-05-31 02:20:56 +0800

    pertama

---
+++ app/src/main/java/com/agungkrisna/applicationtest28/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.agungkrisna.applicationtest28
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
